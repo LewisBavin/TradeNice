d6d521c0efcc4eb5de95b014799aeda7df5ba4a3
LewisBavin <144824398+LewisBavin@users.noreply.github.com>
2024-03-19 05:46:35 +0000

    improved trading

--- GasAndPower/src/Components/Dashbaord/Requests.jsx
+++ GasAndPower/src/Components/Dashbaord/Requests.jsx
@@ -1,40 +1,54 @@
 import React from "react";
 import Request from "./Request";
-import { getGasUsers } from "../../Utilities/Slices/SystemSlice";
+import { gasUsers } from "../../Utilities/systemUsers";
 
+const Requests = ({ reqs, reverse, yourID }) => {
+  const users = gasUsers.map(({ id, name }) => {
+    return { id, name };
+  });
 
+  const counterParties = gasUsers.map(({ id, name }) => {
+    return { id, name };
+  });
 
-const Requests = ({ reqs, counterParty }) => {
-
-    console.log('test')
   const buys = reqs.filter((req) => {
     return req.direction === "input";
   });
   const sells = reqs.filter((req) => {
     return req.direction == "output";
   });
-  console.log("reqs", reqs);
-  console.log("buys: ", buys, "sells: ", sells);
+  console.log(reverse ? "counter" : "you")
+  console.log(  "buys: ", buys, "sells: ", sells);
 
   return (
-    <div className={"requests flx col" + (counterParty ? "-r" : "")}>
+    <div className={"flx col" + (reverse ? "-r" : "")}>
       <div className="inputs">
         <div className="header">Buys</div>
-        <div className="counterParty">
-          <div className="header">counter</div>
-          <div className="requestList">
-            {buys.map((req, i) => {
-              return (
-                <ul key={i}>
-                  <Request />
-                </ul>
-              );
-            })}
-          </div>
+        <div className="requests">
+          {users.map((user) => {
+            return (
+              <>
+                {((!reverse && user.id !== yourID) ||
+                  (reverse && user.id == yourID)) && (
+                  <div className="counterParty">
+                    <div className="header">{user.name}</div>
+                    {buys
+                      .filter((req) => {
+                        return req.counterID == user.id;
+                      })
+                      .map((req) => {
+                        return (
+                          <>
+                            <div>{req.volume}</div>
+                          </>
+                        );
+                      })}
+                  </div>
+                )}
+              </>
+            );
+          })}
         </div>
-      </div>
-      <div className="inputs">
-        <div className="header">Sells</div>
       </div>
     </div>
   );
--- GasAndPower/src/Components/Dashbaord/Trading/TradingOutstanding.jsx
+++ GasAndPower/src/Components/Dashbaord/Trading/TradingOutstanding.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { systemRequests } from "../../../Utilities/systemTransactions";
-
 import Requests from "../Requests";
 
 const TradingOutstanding = ({ userID }) => {
@@ -12,11 +11,11 @@ const TradingOutstanding = ({ userID }) => {
       <div className="requestsContainer flx jc-sb">
         <div>
           <div className="header">Your Requests</div>
-          <Requests reqs={yours} />
+          <Requests reqs={yours} yourID={userID} />
         </div>
         <div>
           <div className="header">Counterparty Requests</div>
-          <Requests reqs={theirs} counterParty={true} />
+          <Requests reqs={theirs} yourID={userID} reverse={true} />
         </div>
       </div>
     </>
