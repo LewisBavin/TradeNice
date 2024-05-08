4744f62bf06520183f07dfc016d9038ada133dfb
LewisBavin <144824398+LewisBavin@users.noreply.github.com>
2024-05-08 22:54:27 +0100

    finished dispute raising and dispute responding

--- src/Components/Dashbaord/3_Trading/Dispute.jsx
+++ src/Components/Dashbaord/3_Trading/Dispute.jsx
@@ -20,7 +20,7 @@ const Dispute = ({ account, users }) => {
   };
   const dispatch = useDispatch();
   const [matched, setMatched] = useState([]);
-  const [splits, setSplits] = useState({ You: [], Counterparty: [] });
+  const [splits, setSplits] = useState(splitInit);
   const [dates, setDates] = useState({
     start_date: format(new Date(), "yyyy-MM-dd"),
     end_date: format(new Date(), "yyyy-MM-dd"),
@@ -67,16 +67,10 @@ const Dispute = ({ account, users }) => {
     setMatched(disputes);
   };
 
-  let toggleCheck = (key, i) => {
-    let temp = [...splits[key]];
-    temp[i].checked = !temp[i].checked;
-    setSplits({ ...splits, [key]: temp });
-  };
-
-  let addComment = (e, key, i) => {
-    let temp = [...splits[key]];
-    temp[i].comment = e.target.value;
-    setSplits({ ...splits, [key]: temp });
+  let addComment = (e, key, innerKey, k) => {
+    let temp = { ...splits };
+    temp[key][innerKey][k].reject_comment = e.target.value;
+    setSplits(temp);
   };
 
   let userName = (userID) =>
@@ -87,76 +81,83 @@ const Dispute = ({ account, users }) => {
   let submit = matched.some((t) => t.checked);
   let haultSubmit = matched.some((t) => t.checked && !t.comment);
 
-  let submitChanges = async () => {
-    if (submit && !haultSubmit)
-      try {
-        let { response } = (
-          await axios.post(
-            "http://localhost:6002/user/requests/raiseDispute",
-            { disputes: matched.filter((t) => t.checked) },
-            {
-              headers: { token: account.user.token },
-            }
-          )
-        ).data;
-        if (response) {
-          dispatch(
-            accountActions.setToast({
-              trigger: true,
-              strong: "Success!",
-              small: "disputes raised with counterparty",
-              body: "head over to disputes to check progress",
-              variant: "success",
-              delay: 5000,
-            })
-          );
-        } else {
-          dispatch(
-            accountActions.setToast({
-              trigger: true,
-              strong: "Error!",
-              body: "Error raising dispures",
-              variant: "danger",
-            })
-          );
-        }
-      } catch (e) {
+  let submitChanges = async (e, key, innerKey, k, bool) => {
+    let trade = { ...splits }[key][innerKey][k];
+    try {
+      let { response, err } = (
+        await axios.post(
+          `http://localhost:6002/user/requests/${
+            bool ? "acceptDispute" : "rejectDispute"
+          }`,
+          { trade },
+          {
+            headers: { token: account.user.token },
+          }
+        )
+      ).data;
+      if (response) {
+        dispatch(
+          accountActions.setToast({
+            trigger: true,
+            strong: "Success!",
+            small: "dispute " + bool ? "accepted" : "rejected",
+            body: bool
+              ? "trade has been removed from the system"
+              : "trade will be honoured and remain in the sytem",
+            variant: "success",
+            delay: 5000,
+          })
+        );
+      } else if (err) {
         dispatch(
           accountActions.setToast({
             trigger: true,
             strong: "Error!",
-            small: "connection error",
-            body: "please contact admin",
-            variant: "warning",
+            body: "Error responding to dispute",
+            variant: "danger",
           })
         );
       }
+    } catch (e) {
+      dispatch(
+        accountActions.setToast({
+          trigger: true,
+          strong: "Error!",
+          small: "connection error",
+          body: "please contact admin",
+          variant: "warning",
+        })
+      );
+    }
   };
 
   let content = {
     Pending: {
       You: {
         msg: "Counterparty has yet to respond to your dispute.",
-        label: "your dispute:",
-        commentKey: "comment",
+        label1: "your dispute:",
+        label2: "their reply:",
       },
       Counterparty: {
         msg: "Please review this counterparty's dispute.",
-        label: "their reply:",
-        commentKey: "comment",
+        label1: "their dispute:",
+        label2: "your reply:",
+
         edit: true,
       },
     },
     Rejected: {
+      label1: "dispute:",
+      label2: "rejection:",
       You: {
-        msg: "You have rejected this counterparty's dispute",
-        label: "your",
-        commentKey: "reject_comment",
+        msg: "Counterparty has rejected your dispute:",
+        label1: "your dispute:",
+        label2: "their rejection:",
       },
       Counterparty: {
-        msg: "Counterparty has rejected your dispute:",
-        label: "their",
-        commentKey: "reject_comment",
+        msg: "You have rejected this counterparty's dispute",
+        label1: "their dispute:",
+        label2: "your rejection:",
       },
     },
   };
@@ -228,131 +229,164 @@ const Dispute = ({ account, users }) => {
       <Container>
         <Accordion>
           {Object.entries(splits).map(([key, innerObj], i) => (
-            <Accordion.Item key={i}>
-              <Accordion.Header>{key + " Disputes"}</Accordion.Header>
-              <Accordion.Body>
-                <Accordion>
+            <Accordion key={i}>
+              <Accordion.Item>
+                <Accordion.Header>
+                  {key + " Disputes - Trades Still Honoured"}
+                </Accordion.Header>
+                <Accordion.Body>
                   {Object.entries(innerObj).map(([innerKey, arr], j) => (
-                    <Accordion.Item key={j}>
-                      <Accordion.Header>
-                        {"Raised By " + innerKey}
-                      </Accordion.Header>
-                      <Accordion.Body>
-                        {arr.map((trade, k) => (
-                          <Form key={k} className=" col ai-c jc-c">
-                            <div className="flx ai-c jc-c">
-                              <FloatingLabel label="Id">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={trade.trade_id}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="User Requested">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={userName(trade.user_id)}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="Dir">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={trade.direction}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="Counter Accepted">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={userName(trade.counter_id)}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="Start">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={format(
-                                    trade.start_date,
-                                    "yyyy-MM-dd"
-                                  )}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="End">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={format(
-                                    trade.end_date,
-                                    "yyyy-MM-dd"
-                                  )}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="Vol / Day">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={trade.volume}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="Price">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={trade.price}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel label="Tot Vol">
-                                <Form.Control
-                                  disabled
-                                  defaultValue={trade.total_volume}
-                                />
-                              </FloatingLabel>
-                              <FloatingLabel
-                                label="Dispute Raised By"
-                                style={{ minWidth: "160px" }}
-                              >
-                                <Form.Control
-                                  disabled
-                                  defaultValue={userName(trade.dispute_user_id)}
-                                />
-                              </FloatingLabel>
-                            </div>
-
-                            <div>
-                              <Form.Text>
-                                {content[key][innerKey].msg}
-                              </Form.Text>
-                              <div className="flx jc-c">
-                                <FloatingLabel
-                                  label={"your " + content[key][innerKey].label}
-                                >
+                    <Accordion key={j}>
+                      <Accordion.Item>
+                        <Accordion.Header>
+                          {"Raised By " + innerKey}
+                        </Accordion.Header>
+                        <Accordion.Body>
+                          {arr.map((trade, k) => (
+                            <Form key={k} className=" col ai-c jc-c">
+                              <div className="flx ai-c jc-c">
+                                <FloatingLabel label="Id">
                                   <Form.Control
-                                    disabled={!content[key][innerKey].edit}
-                                    value={
-                                      trade[content[key][innerKey].commentKey]
-                                    }
-                                    onChange={(e) =>
-                                      addComment(e, key, innerKey, k)
-                                    }
+                                    disabled
+                                    defaultValue={trade.trade_id}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="User Requested">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={userName(trade.user_id)}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="Dir">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={trade.direction}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="Counter Accepted">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={userName(trade.counter_id)}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="Start">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={format(
+                                      trade.start_date,
+                                      "yyyy-MM-dd"
+                                    )}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="End">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={format(
+                                      trade.end_date,
+                                      "yyyy-MM-dd"
+                                    )}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="Vol / Day">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={trade.volume}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="Price">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={trade.price}
+                                  />
+                                </FloatingLabel>
+                                <FloatingLabel label="Tot Vol">
+                                  <Form.Control
+                                    disabled
+                                    defaultValue={trade.total_volume}
                                   />
                                 </FloatingLabel>
                                 <FloatingLabel
-                                  label={content[key][innerKey].label}
+                                  label="Dispute Raised By"
+                                  style={{ minWidth: "160px" }}
                                 >
                                   <Form.Control
-                                    disabled={!content[key][innerKey].edit}
-                                    value={
-                                      trade[content[key][innerKey].commentKey]
-                                    }
-                                    onChange={(e) =>
-                                      addComment(e, key, innerKey, k)
-                                    }
+                                    disabled
+                                    defaultValue={userName(
+                                      trade.dispute_user_id
+                                    )}
                                   />
                                 </FloatingLabel>
                               </div>
-                            </div>
-                          </Form>
-                        ))}
-                      </Accordion.Body>
-                    </Accordion.Item>
+
+                              <div>
+                                <Form.Text>
+                                  {content[key][innerKey].msg}
+                                </Form.Text>
+                                <div className="flx jc-c">
+                                  <FloatingLabel
+                                    label={content[key][innerKey].label1}
+                                  >
+                                    <Form.Control
+                                      disabled
+                                      as="textarea"
+                                      defaultValue={trade.comment}
+                                    />
+                                  </FloatingLabel>
+                                  <FloatingLabel
+                                    label={content[key][innerKey].label2}
+                                  >
+                                    <Form.Control
+                                      disabled={!content[key][innerKey].edit}
+                                      as="textarea"
+                                      value={trade.reject_comment}
+                                      onChange={(e) =>
+                                        addComment(e, key, innerKey, k)
+                                      }
+                                    />
+                                  </FloatingLabel>
+                                  {content[key][innerKey].edit && (
+                                    <>
+                                      <Button
+                                        variant="success"
+                                        onClick={(e) =>
+                                          submitChanges(
+                                            e,
+                                            key,
+                                            innerKey,
+                                            k,
+                                            true
+                                          )
+                                        }
+                                      >
+                                        Accept
+                                      </Button>
+                                      <Button
+                                        variant="danger"
+                                        onClick={(e) =>
+                                          submitChanges(
+                                            e,
+                                            key,
+                                            innerKey,
+                                            k,
+                                            false
+                                          )
+                                        }
+                                      >
+                                        Reject
+                                      </Button>
+                                    </>
+                                  )}
+                                </div>
+                              </div>
+                            </Form>
+                          ))}
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
                   ))}
-                </Accordion>
-              </Accordion.Body>
-            </Accordion.Item>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           ))}
         </Accordion>
 
